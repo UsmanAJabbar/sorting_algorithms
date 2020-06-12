10fbd197bd8e076624a8e1f2c2984d4906313366
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-06-12 20:10:02 +0000

    Still not working but need to review up on the whiteboard

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 
+void swapper(listint_t **list, listint_t *sorted);
 /**
  *
  *
@@ -9,13 +10,11 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *check_sorted, *sorted = *list;
+	listint_t *temp, *sorted = *list;
 
-	/* Well need to compare two nodes and the is a > b then b will swap
-	// is a<b then we will compare b and c. b > c swap and c < a swap continue
+	/* Well need to compare two nodes and the if a > b then b will swap
+	// if a<b then we will compare b and c. b > c swap and c < a swap continue
 	// swapping until in the lowest place for that node->n.
-
-	 if position->n > position->next->n then *tmp = postion->next
 	*/
 	while (sorted != NULL)
 	{
@@ -24,39 +23,41 @@ void insertion_sort_list(listint_t **list)
 		/* move one past the sorted section of the list */
 		if (temp->n < sorted->n)
 		{
-			/* If temp.n is greater than sorted.n pull temp out of the list */
-			sorted->next = temp->next;
-			temp->next->prev = sorted;
-			/* This stitches the sorten noted to temp next and temp is now free floating*/
-			if (sorted->prev != NULL)
-			{
-				check_sorted = sorted;
-				while (check_sorted->n > temp->n && check_sorted->prev != NULL)
-					check_sorted = check_sorted->prev;
-				if (check_sorted->prev == NULL)
-				{
-					temp->prev = NULL;
-					temp->next = check_sorted;
-					check_sorted->prev = temp;
-				}
-				else
-				{
-					temp->next = check_sorted->next;
-					temp->next->prev = temp;
-					temp->prev = check_sorted;
-					check_sorted->next = temp;
-				}
-			}
-			else
-			{
-				temp->prev = NULL;
-				temp->next = sorted;
-				sorted->prev = temp;
-			}
-			print_list(*list);
+			swapper(list, sorted);
 		}
 		else
 			sorted = sorted->next;
 	}
+}
 
+
+
+void swapper(listint_t **list, listint_t *sorted)
+{
+	listint_t *check_sorted, *temp = sorted->next;
+
+/* First we are going to swap sorted and temp as this function is called when
+temp->n is < sort->n in the above function*/
+	sorted->next = temp->next;
+	temp->next->prev = sorted;
+       	temp->next = sorted;
+	if (sorted->prev !=NULL)
+	{
+		sorted->prev->next = temp;
+		temp->prev = sorted->prev;
+	}
+	sorted->prev = temp;
+	print_list(*list); 
+
+        if (temp->prev != NULL)
+	{
+		check_sorted = temp->prev;
+		if (check_sorted->n > temp->n)
+			swapper(list, check_sorted);
+	}
+	else
+	{
+		(void) check_sorted;
+		(*list) = temp;
+	}
 }
