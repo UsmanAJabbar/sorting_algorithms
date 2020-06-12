0796ab9672069d331d14244f73c2807cb70153f0
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-06-12 14:45:50 +0000

    Adding non-working insertion sort so I can pull it up on mobile and white board

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,23 +11,23 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp, *check_sorted, *sorted = *list;
 
-	// Well need to compare two nodes and the is a > b then b will swap
+	/* Well need to compare two nodes and the is a > b then b will swap
 	// is a<b then we will compare b and c. b > c swap and c < a swap continue
 	// swapping until in the lowest place for that node->n.
 
-	// if position->n > position->next->n then *tmp = postion->next
-	
+	 if position->n > position->next->n then *tmp = postion->next
+	*/
 	while (sorted != NULL)
 	{
-		// While we are not through the end of our list
+		/* While we are not through the end of our list */
 		temp = sorted->next;
-		// move one past the sorted section of the list
-		if (temp->n > sorted->n)
+		/* move one past the sorted section of the list */
+		if (temp->n < sorted->n)
 		{
-			// If temp.n is greater than sorted.n pull temp out of the list
+			/* If temp.n is greater than sorted.n pull temp out of the list */
 			sorted->next = temp->next;
 			temp->next->prev = sorted;
-			// This stitches the sorten noted to temp next and temp is now free floating
+			/* This stitches the sorten noted to temp next and temp is now free floating*/
 			if (sorted->prev != NULL)
 			{
 				check_sorted = sorted;
@@ -37,6 +37,7 @@ void insertion_sort_list(listint_t **list)
 				{
 					temp->prev = NULL;
 					temp->next = check_sorted;
+					check_sorted->prev = temp;
 				}
 				else
 				{
@@ -50,12 +51,12 @@ void insertion_sort_list(listint_t **list)
 			{
 				temp->prev = NULL;
 				temp->next = sorted;
-				sorted->prev = temp
+				sorted->prev = temp;
 			}
-			sorted = sorted->next;
+			print_list(*list);
 		}
 		else
-			sorted = sorted->next
+			sorted = sorted->next;
 	}
 
 }
